f5ebb905d872f6c163649a9c43b07c89a7dafe26
Leonel Rivas <leonrivas04@gmail.com>
2022-05-19 10:57:00 -0600

    aplicando ngSwitch y ngClass

--- src/app/modules/task/task.module.ts
+++ src/app/modules/task/task.module.ts
@@ -5,13 +5,15 @@ import { TaskRoutingModule } from './task-routing.module';
 import { TaskPageComponent } from './pages/task-page.component';
 import { HeaderComponent } from './components/header/header.component';
 import { IfDirectiveComponent } from './components/if-directive/if-directive.component';
+import { SwitchDirectiveComponent } from './components/switch-directive/switch-directive.component';
 
 
 @NgModule({
   declarations: [
     TaskPageComponent,
     HeaderComponent,
-    IfDirectiveComponent
+    IfDirectiveComponent,
+    SwitchDirectiveComponent
   ],
   imports: [
     CommonModule,
